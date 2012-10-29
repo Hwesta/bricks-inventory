1d5c9d8a44534ee9d116d3948099713b1574f704
Holly Becker <becker.holly@gmail.com>
2012-10-28 17:00:58 -0700

    Inventory can be saved!

--- inventory/views.py
+++ inventory/views.py
@@ -15,27 +15,31 @@ def index(request):
 def add_inventory(request):
     """ Adds a piece of inventory. """
     if request.method == 'POST':
-        form = InventoryForm(request.POST)
-        if form.is_valid():
-            part = form.cleaned_data['part']
-            color = form.cleaned_data['color']
-            count = form.cleaned_data['count']
+        inventory_form = InventoryForm(request.POST, prefix="inv")
+        #kw_form = KeywordForm(request.POST, prefix="kw")
+        if inventory_form.is_valid():
+            part_str = inventory_form.cleaned_data['part']
+            color = inventory_form.cleaned_data['color']
+            count = inventory_form.cleaned_data['count']
 
-            print "part", part, "color type", type(color)
-            part_obj = Part.objects.get(part_id__iexact=part)
-            print "part_obj", part_obj, type(part_obj)
-            partinstance = PartInstance.objects.get(part=part_obj, color=color)
-            # HELP color+part do not uniquely define a partinstance
-            # codename actually uniquely defines a partinstance, and color+part are actually useless
-            # How to determine which version of the partinstance (same color+part) to attaching Inventory to?
-            # Should we keep the codenames??
-
-            # partinstance = PartInstance.objects.filter(part=part_obj)#, color=color)
+            part = Part.objects.get(part_id__iexact=part_str)
+            try:
+                partinstance = PartInstance.objects.get(part=part, color=color)
+            except PartInstance.DoesNotExist:
+                print "PartInstance not exist, creating"
+                partinstance = PartInstance(color=color, part=part, user_override=True)
+                partinstance.save()
             print "partinstance", partinstance
-            #i = Inventory(partinstance=partinstance, count=count)
-            #i.save()
+            i = Inventory(partinstance=partinstance, count=count)
+            print "inventory", i
+            i.save()
             return HttpResponseRedirect(reverse(index))
     else:
-        form = InventoryForm()
-    return render(request, 'add_inventory.html', {'form': form, })
+        inventory_form = InventoryForm(prefix="inv")
+        #KeywordFormSet = modelformset_factory(KeywordValue, form=KeywordForm, extra=0)
+        #kw_formset = KeywordFormSet(queryset=Keyword.objects.all(), prefix="kw")
+    return render(request, 'add_inventory.html',
+        {'inventory_form': inventory_form,
+        # 'kw_formset': kw_formset,
+        })
 
